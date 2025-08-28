02cbebd180da43cdbcda02275e727f51a104c025
StrazzlDazzle <odogstraz@gmail.com>
2025-08-28 11:19:15 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Authors: Owen Strazdas, Nino Gergonne
+ *
+ * This code prints the names and Majors of Owen and Nino
+'*/
+
+int main(int argc char **argv) {
+
+    printf("Hello World, My name is Owen Strazdas\nMy Major is Electrical Elctrical Engineering");
+
+    return 0;
+
+}
+
+
