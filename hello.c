f11b07978153bebf2e16966c1c607fdd0db8c0fd
StrazzleDazzle <odogstraz@gmail.com>
2025-08-30 22:14:26 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Authors: Owen Strazdas, Nino Gergonne
+ *
+ *Simple C code that prints the names and Majors of Owen and Nino
+'*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World, My name is Owen Strazdas!\n");
+
+    return 0;
+
+}
